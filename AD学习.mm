1d60cf72ca651649d2ca797a3728b33c9e110021
guozhenjiang <guozhenjiang@bj-zkhd.com>
2017-12-30 00:53:45 +0800

    2017-12-30 00:53:51 郭振江 学习了阻焊开窗、板子开洞。

---
+++ AD学习.mm
@@ -0,0 +1,37 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1514564244474" ID="ID_1836919977" MODIFIED="1514566021269" TEXT="AD&#x5b66;&#x4e60;">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1514565794013" ID="ID_604783684" MODIFIED="1514566021269" POSITION="right" TEXT="&#x57fa;&#x7840;">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1514565736128" ID="ID_1884632851" MODIFIED="1514566021269" TEXT="&#x677f;&#x5b50;&#x5916;&#x5f62;">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1514565752206" ID="ID_1051405760" MODIFIED="1514566021269" TEXT="&#x8bbe;&#x8ba1;">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1514565779565" ID="ID_1891781724" MODIFIED="1514566021269" TEXT="&#x677f;&#x5b50;&#x5916;&#x5f62;">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+<node CREATED="1514564691384" ID="ID_1984362832" MODIFIED="1514566021269" TEXT="&#x677f;&#x5b50;&#x5f00;&#x6d1e;">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1514565822021" ID="ID_827116478" MODIFIED="1514566021269" TEXT="&#x753b;&#x5c01;&#x95ed;&#x7ebf;&#x6761;">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1514565842771" ID="ID_1356347350" MODIFIED="1514566021269" TEXT="&#x5de5;&#x5177;">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1514565978397" ID="ID_309431473" MODIFIED="1514566021269" TEXT="&#x8f6c;&#x6362;">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1514565984435" ID="ID_1205386394" MODIFIED="1514566021269" TEXT="&#x6839;&#x636e;&#x5df2;&#x9009;&#x62e9;&#x9879;&#x526a;&#x5207;&#x677f;&#x5b50;">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1514566044664" ID="ID_1982002860" MODIFIED="1514566066574" TEXT="&#x963b;&#x710a;&#x5f00;&#x7a97;">
+<node CREATED="1514566066574" ID="ID_1662953807" MODIFIED="1514566090945" TEXT="&#x963b;&#x710a;&#x5c31;&#x662f;&#x5e26;&#x989c;&#x8272;&#x7684;&#x6cb9;"/>
+<node CREATED="1514566092560" ID="ID_759490567" MODIFIED="1514566335051" TEXT="&#x5c06;&#x6240;&#x753b;&#x5f62;&#x72b6;&#x8bbe;&#x7f6e;&#x4e3a; Top Solder&#xff08;Bottom Solder&#xff09; &#x5373;&#x53ef;"/>
+</node>
+</node>
+</node>
+</map>
